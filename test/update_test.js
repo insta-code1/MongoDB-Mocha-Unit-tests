91c85dd721b59f7174f1cbb8e0114562ef37c9db
insta-code1 <test@insta-code.io>
2017-03-30 22:42:58 +0100

    Testing validators and operators

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe' });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save()
       .then(() => done());
   });
@@ -50,5 +50,15 @@ describe('Updating records', () => {
       done
       );
   });
+
+  it('A user can have their postCount incremented by 1', (done) => {
+    User.update({name: 'Joe'}, { $inc: { postCount: 100 } })
+      .then(() => User.findOne({ name: 'Joe'}))
+      .then((user) => {
+        assert(user.postCount === 100);
+        done()
+      });
+  });
+
 
 });
